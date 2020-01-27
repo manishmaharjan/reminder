0d2a299b881e38c39420a680a7828cd7df3853ef
Manish <manish_0977@hotmail.com>
2020-01-27 23:54:22 +0200

    added functionality for main activity, time activity and map activity

--- app/src/main/java/com/mobilecomputing/reminder/MainActivity.kt
+++ app/src/main/java/com/mobilecomputing/reminder/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.mobilecomputing.reminder
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab_add.setOnClickListener {
+            Toast.makeText(this, "This is main activity", Toast.LENGTH_SHORT).show()
+        }
+
+        fab_time.setOnClickListener {
+            val fabTimeIntent = Intent (applicationContext, TimeActivity::class.java)
+            startActivity(fabTimeIntent)
+        }
+
+        fab_map.setOnClickListener {
+            val fabMapIntent = Intent (applicationContext, MapActivity::class.java)
+            startActivity(fabMapIntent)
         }
     }
-
+    /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -35,5 +45,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
+    } */
 }
