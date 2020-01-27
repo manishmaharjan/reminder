808282e39a5d2112ffae7b530784b656723a21d7
Manish <manish_0977@hotmail.com>
2020-01-27 21:41:51 +0200

    time activity added

---
+++ app/src/main/java/com/mobilecomputing/reminder/TimeActivity.kt
@@ -0,0 +1,12 @@
+package com.mobilecomputing.reminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time)
+    }
+}
